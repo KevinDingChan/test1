30e515ff7f2a01f975672de8a0377e91f7316d2c
KevinDingChan <kevindingchan@outlook.com>
2019-10-14 08:51:11 +0800

    ttttt

---
+++ cmake-build-debug/CMakeFiles/agri_01.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/agri_01.dir/main.cpp.o"
+  "agri_01.pdb"
+  "agri_01"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/agri_01.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
